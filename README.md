4481caa392443f540993a9b95a82f8f226fe8584
badgerrr <jackarthurton@gmail.com>
2020-04-10 14:45:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Flask Forecasting Application
+
+##TODO
+
+- [x] Setup HelloWorld Flask
+- [ ] Get Pycharm working with docker-compose
+- [ ] Build basic html template 
+- [ ] Investigate socketio # dash-forecasting
+# dash-forecasting
---
+++ app.py
@@ -0,0 +1,71 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Output, Input
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+            Dash: A web application framework for Python.
+        '''),
+
+    html.Label('Slider'),
+    dcc.Slider(
+        id='myslider',
+        min=-10,
+        max=10,
+        step=1,
+        marks={i: '{}'.format(i) for i in range(-10, 11) if i % 1 == 0},
+        value=0,
+    ),
+    dcc.Graph(
+        id='example-graph'
+    ),
+    dcc.Graph(
+        id='example-graph2'
+    ),
+])
+
+
+def main():
+    app.run_server(host='0.0.0.0',debug=True)
+
+
+@app.callback(
+    inputs=[Input(component_id='myslider', component_property='value')],
+    output=Output(component_id='example-graph2', component_property='figure')
+)
+def dynamic_graph(slider_value):
+    return {
+        'data': [
+            {'x': [1, 2, 3], 'y': [int(slider_value), 1, 2], 'name': 'SF', 'marker': {'color': 'rgb(26, 118, 255)'}},
+            {'x': [1, 2, 3], 'y': [2, 4, 5], 'name': u'Montréal', 'marker': {'color': 'rgb(55, 83, 109)'}},
+        ],
+        'layout': {
+            'title': 'Dash Data Visualization'
+        }
+    }
+
+@app.callback(
+    inputs=[Input(component_id='myslider', component_property='value')],
+    output=Output(component_id='example-graph', component_property='figure')
+)
+def dynamic_graph(slider_value):
+    return {
+        'data': [
+            {'x': [1, 2, 3], 'y': [int(slider_value), 1, 2], 'type': 'bar', 'name': 'SF'},
+            {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+        ],
+        'layout': {
+            'title': 'Dash Data Visualization'
+        }
+    }
+
+
+if __name__ == '__main__':
+       main()
